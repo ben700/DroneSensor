c21afd10f2483d8a51e044e1012e5409e9764e89
ben700 <ben700@hotmail.com>
2021-02-12 21:19:59 +0000

    Update DroneSensor.ino

--- examples/DroneSensor/DroneSensor.ino
+++ examples/DroneSensor/DroneSensor.ino
@@ -3,26 +3,25 @@
 
 #define DroneSensor_debug false
 
-  
 DroneSensor *sensors;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  
-  sensors = new DroneSensor(WiFi.macAddress(), WiFi.localIP().toString());
+  Serial.begin(115200);  
+  sensors = new DroneSensor(WiFi.macAddress(), WiFi.localIP().toString(), "DeviceID");
 
 }
 
 void loop() {
   
+  Serial.println("Testing...");
   sensors->list_devices();
 
+  sensors->bootPayload("Time");
  
+  sensors->deviceStatePayload();
+  sensors->sensorPayload("Time");
+  sensors->calibrationCommands();
 
-sensors->deviceStatePayload();
-sensors->sensorPayload("Time");
-sensors->calibrationCommands();
-
-delay(10000);
+  delay(10000);
 }
