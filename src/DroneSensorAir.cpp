5d82c749243afebeb2cedde9ced6e91461e0655a
ben700 <ben700@hotmail.com>
2021-03-09 11:37:08 +0000

    Update DroneSensorAir.cpp

--- src/DroneSensorAir.cpp
+++ src/DroneSensorAir.cpp
@@ -1,4 +1,4 @@
-#include "DroneSensor.h"
+#include "DroneSensorAir.h"
 
 DroneSensor::DroneSensor(String __deviceMAC, String __deviceIP, String __deviceID, bool _DroneSensor_debug = false)
 {
@@ -7,14 +7,6 @@ DroneSensor::DroneSensor(String __deviceMAC, String __deviceIP, String __deviceI
   this->_deviceIP = __deviceIP;
   this->_deviceID = __deviceID;
   Wire.begin(); 
-  pinMode(EN_PH, OUTPUT);                                                         //set enable pins as outputs
-  pinMode(EN_EC, OUTPUT);
-  pinMode(EN_RTD, OUTPUT);
-  pinMode(EN_AUX, OUTPUT);
-  digitalWrite(EN_PH, LOW);                                                       //set enable pins to enable the circuits
-  digitalWrite(EN_EC, LOW);
-  digitalWrite(EN_RTD, HIGH);
-  digitalWrite(EN_AUX, LOW); 
   if (DroneSensor_debug) { Serial.println(F("DroneSensor::DroneSensor()"));}
   delay(long_delay);
   
@@ -46,15 +38,7 @@ DroneSensor::DroneSensor(String __deviceMAC, String __deviceIP, String __deviceI
   setFallbackTemp(this->_FallbackTemp);
 }
 
-void DroneSensor::setFallbackTemp(float __FallbackTemp){
-  this->_FallbackTemp = __FallbackTemp;
-  for (int i = 0; i < device_list_len; i++ )
-  {
-    if(device_list[i].tempCompensation){
-      device_list[i].device.send_cmd_with_num("T,", this->_FallbackTemp);
-    }
-  }
-}
+
 
 void DroneSensor::debug() {
   DroneSensor_debug=true;
@@ -98,44 +82,10 @@ String DroneSensor::lookupLedStatus(String LED){
   if (LED == "1"){ return "On";}else{return "Off";}
 }
 
-
-void DroneSensor::get_ec_k_value(){                                    //function to query the value of the ec circuit
-  char rx_buf[10];                                        //buffer to hold the string we receive from the circuit
-  EC.send_cmd("k,?");                                     //query the k value
-  delay(300);
-  if(EC.receive_cmd(rx_buf, 10) == Ezo_board::SUCCESS){   //if the reading is successful
-    k_val = String(rx_buf).substring(3).toFloat();        //parse the reading into a float
-  }
-}
-
-
 void DroneSensor::turnParametersOn() {
   this->parametersOn = true;
   for (int i = 0; i < device_list_len; i++) {
-    if(device_list[i].device.get_name() == "conductivity" and device_list[i]._status == EZOStatus::Connected){
-      if (DroneSensor_debug) {Serial.print(F("Conductivity is "));}
-      String command = "O,EC,1";
-      device_list[i].device.send_cmd(command.c_str());
-      select_delay(command);
-      command = "O,TDS,1";
-      device_list[i].device.send_cmd(command.c_str());
-      select_delay(command);
-      command = "O,S,1";
-      device_list[i].device.send_cmd(command.c_str());
-      select_delay(command);
-      command = "O,SG,1";
-      device_list[i].device.send_cmd(command.c_str());
-      select_delay(command);
-      if (DroneSensor_debug) {Serial.println(F("reconfigured!"));}
-    }else if (device_list[i].device.get_name() == "DO" and device_list[i]._status == EZOStatus::Connected){
-      if (DroneSensor_debug) { Serial.print(F("Dissolved Oxygen is "));}
-      String command = "O,mg,1";
-      device_list[i].device.send_cmd(command.c_str());
-      select_delay(command);
-      command = "O,%,1";
-      device_list[i].device.send_cmd(command.c_str());
-      if (DroneSensor_debug) {Serial.println(F("reconfigured!"));}
-    }else if (device_list[i].device.get_name() == "CO2" and device_list[i]._status == EZOStatus::Connected){
+  if (device_list[i].device.get_name() == "CO2" and device_list[i]._status == EZOStatus::Connected){
       if (DroneSensor_debug) {Serial.print(F("CO2 is "));}
       String command = "O,t,1";
       device_list[i].device.send_cmd(command.c_str());
@@ -157,30 +107,7 @@ void DroneSensor::turnParametersOn() {
 void DroneSensor::turnParametersOff() {
   this->parametersOn = false;
   for (int i = 0; i < device_list_len; i++) {
-    if(device_list[i].device.get_name() == "conductivity" and device_list[i]._status == EZOStatus::Connected){
-      if (DroneSensor_debug) {Serial.print(F("Conductivity is "));}
-      String command = "O,EC,1";
-      device_list[i].device.send_cmd(command.c_str());
-      select_delay(command);
-      command = "O,TDS,0";
-      device_list[i].device.send_cmd(command.c_str());
-      select_delay(command);
-      command = "O,S,0";
-      device_list[i].device.send_cmd(command.c_str());
-      select_delay(command);
-      command = "O,SG,0";
-      device_list[i].device.send_cmd(command.c_str());
-      select_delay(command);
-      if (DroneSensor_debug) {Serial.println(F("reconfigured!"));}
-    }else if (device_list[i].device.get_name() == "DO" and device_list[i]._status == EZOStatus::Connected){
-      if (DroneSensor_debug) {Serial.print(F("Dissolved Oxygen is "));}
-      String command = "O,mg,1";
-      device_list[i].device.send_cmd(command.c_str());
-      select_delay(command);
-      command = "O,%,0";
-      device_list[i].device.send_cmd(command.c_str());
-      if (DroneSensor_debug) {Serial.println(F("reconfigured!"));}
-    }else if (device_list[i].device.get_name() == "CO2" and device_list[i]._status == EZOStatus::Connected){
+   if (device_list[i].device.get_name() == "CO2" and device_list[i]._status == EZOStatus::Connected){
       if (DroneSensor_debug) {Serial.print(F("CO2 is "));}
       String command = "O,t,0";
       device_list[i].device.send_cmd(command.c_str());
@@ -202,29 +129,7 @@ void DroneSensor::turnParametersOff() {
 
 void DroneSensor::sendReadCommand(StaticJsonDocument<DOC_SIZE>& _doc) {
   if (DroneSensor_debug) { Serial.println(F("DroneSensor::sendReadCommand()"));}
-  float temp = this->_FallbackTemp;
-  if(device_list[0]._status == EZOStatus::Connected){
-    device_list[0].device.send_read_cmd();
-    delay(reading_delay);
-    device_list[0].device.receive_read_cmd();
-    if ((device_list[0].device.get_error() == Ezo_board::SUCCESS) && (device_list[0].device.get_last_received_reading() > -1000.0))
-    {
-      temp =device_list[0].device.get_last_received_reading();
-      if (DroneSensor_debug) { Serial.print(F("DroneSensor::sendReadCommand RTD.get_last_received_reading() ")); Serial.println(temp);}
-    } 
-  
-    for (int i = 0; i < device_list_len; i++ )
-    {
-      if(device_list[i]._status == EZOStatus::Connected){
-        if(device_list[i].tempCompensation){
-          device_list[i].device.send_read_with_temp_comp(temp);
-        }else{
-          device_list[i].device.send_read_cmd();
-        }
-        if (DroneSensor_debug) { Serial.println("DroneSensor::sendReadCommand() -> Sent read command to " + String(device_list[i].device.get_name())); }  
-      }
-    }
-  }else{
+
     for (int i = 0; i < device_list_len; i++ )
     {
       if(device_list[i]._status == EZOStatus::Connected){
@@ -232,7 +137,6 @@ void DroneSensor::sendReadCommand(StaticJsonDocument<DOC_SIZE>& _doc) {
         if (DroneSensor_debug) { Serial.println("DroneSensor::sendReadCommand() -> Sent read command to " + String(device_list[i].device.get_name())); }  
       }
     }
-  }
 }
 
 
@@ -351,11 +255,7 @@ String DroneSensor::deviceStatePayload (){
   for (int i = 0; i < device_list_len; i++ )
   {
     if(device_list[i]._status == EZOStatus::Connected){
-      if(device_list[i].device.get_name() == "conductivity")
-      {
-        get_ec_k_value();
-        doc[device_list[i].device.get_name()]["k Value"] = k_val;
-      }
+     
     //  doc[device_list[i].device.get_name()]["Temperature Compensation"] = device_list[i].tempCompensation;
       doc[device_list[i].device.get_name()]["tempComp"] = device_list[i].tempCompensation;
       singleDeviceStatePayload(device_list[i].device, doc);
@@ -400,13 +300,7 @@ bool DroneSensor::processCommand(StaticJsonDocument<DOC_SIZE>& _command){
    
 bool DroneSensor::processConfig(StaticJsonDocument<DOC_SIZE>& _config){
   bool returnCode = true;
-  if(_config["Fallback Temperature"] != NULL){
-    float __fallbackTemperature = _config["Fallback Temperature"].as<float>();
-    if (DroneSensor_debug) {Serial.println("Setting Fallback Temperature to " + String(__fallbackTemperature));}
-    if(__fallbackTemperature > 0){
-      setFallbackTemp(__fallbackTemperature);
-    }
-  }
+
 
   if(_config["Poll Delay"] != NULL){
     int __pollDelay = _config["Poll Delay"].as<int>();
