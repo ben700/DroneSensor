f0a32351aa7916167bd29f3b90f2912d766655be
ben700 <ben700@hotmail.com>
2021-03-09 02:03:34 +0000

    Update DroneSensor.cpp

--- src/DroneSensor.cpp
+++ src/DroneSensor.cpp
@@ -262,10 +262,7 @@ String DroneSensor::sensorPayload(String _EpochTime)
         for(int y=0; y < countParameter; y++){
           if(pReading != NULL){
             if(device_list[i]._parameterList[y]._payloadName != NULL and device_list[i]._parameterList[y]._payloadName.length() >0){
-              Serial.print("pReading = ");
-              
-              Serial.println(pReading);
-              doc[device_list[i]._parameterList[y]._payloadName] = String(atof(pReading), device_list[i]._parameterList[y]._precision);
+              doc[device_list[i]._parameterList[y]._payloadName] = String(atof(pReading), device_list[i]._parameterList[y]._precision).trim();
             }
           }else{
               if (DroneSensor_debug) {Serial.println("Error: Null but expected to get " + device_list[i]._parameterList[y]._displayName + " for " + String(device_list[i].device.get_name()));}
