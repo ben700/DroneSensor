bd9c591bf07abe64167a44ccf1317ab1c73183e6
ben700 <ben700@hotmail.com>
2021-12-24 14:19:06 +0000

    Update DroneSensor.cpp

--- src/DroneSensor.cpp
+++ src/DroneSensor.cpp
@@ -274,7 +274,7 @@ void DroneSensor::sendReadCommand(StaticJsonDocument<DOC_SIZE> &_doc)
         {
             if (device_list[i]._status == EZOStatus::Connected)
             {
-                if (temp != null && temp >= 0)
+                if (temp != NULL && temp >= 0)
                 {
                     device_list[i].device.send_read_with_temp_comp(temp);
                 }
