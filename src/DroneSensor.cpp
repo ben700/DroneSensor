667670618d0959d54859454d3fa63374af151c1f
ben700 <ben700@hotmail.com>
2021-12-24 18:46:24 +0000

    Update DroneSensor.cpp

--- src/DroneSensor.cpp
+++ src/DroneSensor.cpp
@@ -428,40 +428,8 @@ void DroneSensor::singleDeviceStatePayload(Ezo_board &Device, StaticJsonDocument
         if (Device.get_name() == EC.get_name())
         {
             get_ec_k_value();
-            doc[Device.get_name()]["kValue"] = k_val;
-    
-            command = "O,?";
-            Device.send_cmd(command.c_str());
-            select_delay(command);
-            if (Device.receive_cmd(receive_buffer, 32) == Ezo_board::SUCCESS)
-            {                                      // if the reading is successful
-                cmdReply = String(receive_buffer); // parse the reading into a float
-            }
-
-            if (cmdReply.indexOf(",EC") != -1)
-            {
-                doc[Device.get_name()]["conductivity"] = true;
-            }
-            else
-            {
-                doc[Device.get_name()]["conductivity"] = false;
-            }
-            if (cmdReply.indexOf(",TDS") != -1)
-            {
-                doc[Device.get_name()]["dissolvedSolids"] = true;
-            }
-            else
-            {
-                doc[Device.get_name()]["dissolvedSolids"] = false;
-            }
-            if (cmdReply.indexOf(",S") != -1)
-            {
-                doc[Device.get_name()]["salinity"] = true;
-            }
-            else
-            {
-                doc[Device.get_name()]["salinity"] = false;
-            }
+            doc[Device.get_name()]["kValue"] = k_val;     
+           
         }
         if (Device.get_name() == DO.get_name())
         {
